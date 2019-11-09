b134eca73adb4f282e92d3b2ce67a3dc763193bd
Thiago <skiptirengu@users.noreply.github.com>
2019-11-09 00:37:23 -0300

    Update no-twitter-trending.js

--- no-twitter-trending.js
+++ no-twitter-trending.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name         No twitter trending
-// @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Remove trending thingy from twitter
 // @author       skiptirengu
 // @match        *.twitter.com/*
-// @grant        none
 // @require      https://code.jquery.com/jquery-3.4.1.slim.min.js
 // @updateURL    https://raw.githubusercontent.com/skiptirengu/no-twitter-trending/master/no-twitter-trending.js
 // ==/UserScript==
